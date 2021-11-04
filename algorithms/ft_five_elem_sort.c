07ff2ed9a6515650eab7bc3d17202ddbe9ecec1c
vmusunga <vmusunga@student.s19.be>
2021-11-04 17:33:54 +0100

    4/11

--- algorithms/ft_five_elem_sort.c
+++ algorithms/ft_five_elem_sort.c
@@ -6,7 +6,7 @@
 /*   By: vmusunga <vmusunga@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 17:29:59 by vmusunga          #+#    #+#             */
-/*   Updated: 2021/10/31 02:29:27 by vmusunga         ###   ########.fr       */
+/*   Updated: 2021/11/04 17:22:32 by vmusunga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,15 @@ void	ft_five_elem_sort(t_list **stack_a, t_list **stack_b, int len)
 		ft_two_elem_sort(stack_b);
 	}
 	ft_three_elem_sort(stack_a);
-	//ft_two_elem_sort(stack_b);
-	//ft_pa(stack_a, stack_b, 1);
-
+	while (*stack_b)
+	{
+		while ((*stack_a)->content < (*stack_b)->content)
+			ft_ra(stack_a, 1);
+		ft_pa(stack_a, stack_b, 1);
+		ft_ra(stack_a, 1);
+	}
+	ft_ra(stack_a, 1);
+	if ((*stack_a)->content > (*stack_a)->next->content)
+		ft_ra(stack_a, 1);
 	return ;
 }
