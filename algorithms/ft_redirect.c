5774ce14fadd9f07e391e22e93841cff565648a9
Victor Musungayi <vmusunga@student.s19.be>
2022-04-04 12:41:00 +0200

    4/4

--- algorithms/ft_redirect.c
+++ algorithms/ft_redirect.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_redirect.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vic <vic@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: vmusunga <vmusunga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 15:35:28 by vmusunga          #+#    #+#             */
-/*   Updated: 2022/03/21 16:35:23 by vic              ###   ########.fr       */
+/*   Updated: 2022/04/04 12:37:05 by vmusunga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ void	ft_redirect(t_list **stack_a, t_list **stack_b)
 		ft_hundred_elem_sort(stack_a, stack_b);
 	if (len > 100 && len <= 500)
 		ft_hundred_elem_sort(stack_a, stack_b);
-	//  print_list(*stack_a, *stack_b);
+	// print_list(*stack_a, *stack_b);
 	// ft_output_check(stack_a, 1);
 	return ;
 }
--- draft.c
+++ draft.c
@@ -3,86 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   draft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vic <vic@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: vmusunga <vmusunga@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 12:58:08 by vmusunga          #+#    #+#             */
-/*   Updated: 2022/03/21 16:40:11 by vic              ###   ########.fr       */
+/*   Updated: 2022/04/04 12:02:12 by vmusunga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/push_swap.h"
 
-void	ft_hundred_elem_sort(t_list **stack_a, t_list **stack_b)
-{
-	int i;
-	int len;
-	int chunk;
-	int min_set;
+// void	ft_hundred_elem_sort(t_list **stack_a, t_list **stack_b)
+// {
+// 	int i;
+// 	int len;
+// 	int chunk;
+// 	int min_set;
 
-	min_set = ft_lstmin(stack_a);
-	len = ft_lstsize(*stack_a);
-	chunk = len / 6;
-	i = 0;
-	while (*stack_a && i <= chunk)
-	{
-		// printf("\nmin_set = %d\n", min_set);
-		// printf("\nI = %d\n", i);
-		// printf("chunk = %d\n", chunk);
-		if (i == chunk) 
-		{
-			if (ft_lstmin(stack_b) < ft_lstmin(stack_a))
-			{
-				min_set = ft_lstmax(stack_b);
-				// printf("\nmin_set = %d\n", min_set);
-				ft_nb_to_top(stack_b, ft_lstmax(stack_b));
-				ft_nb_to_top(stack_a, ft_lstmin(stack_a));
-				while (*stack_b)
-				{
-					ft_pa(stack_a, stack_b, 1);
-					i = 0;
-				}
-				ft_wich_min_set(stack_a, min_set);   // TRIES TO send correct min to top of A
-				// print_list(*stack_a, *stack_b);
-			}
-			if (ft_lstmin(stack_a) < ft_lstmax(stack_b))
-			{
-				if (ft_lstmin(stack_a) == min_set)
-					ft_nb_to_top(stack_a, ft_lstmin(stack_a));  
-				else
-					ft_wich_min_set(stack_a, min_set);
-				// printf("\nHERE\n");
-				// print_list(*stack_a, *stack_b);
-				i = 0;
-			}
-			// print_list(*stack_a, *stack_b);
-			// chunk += len / 6;
-		}
-		else
-		{
-			ft_wich_min_set(stack_a, min_set);   //TRIES TO send correct min to top of A
-			// printf("\n BUG?\n");
-		}
-		if (*stack_b)
-		{
-			if ((*stack_a)->content < ft_lstmin(stack_b))
-				ft_nb_to_top(stack_b, ft_lstmax(stack_b));   //ensure the max is on top before pushing
-			if ((*stack_a)->content > ft_lstmin(stack_b) && (*stack_a)->content < ft_lstmax(stack_b))
-				ft_nb_to_top_b(stack_b, ft_lesser_than(*stack_b, (*stack_a)->content));   //if between min & max, find right one
-			if ((*stack_a)->content > ft_lstmax(stack_b))
-				ft_nb_to_top(stack_b, ft_lstmax(stack_b));
-		}
-		if (!(*stack_b) && !ft_output_check(stack_a, 0))
-			return ;
-		ft_pb(stack_a, stack_b, 1);
-		i++;
-		// print_list(*stack_a, *stack_b);
-	}
-	while (*stack_b)
-		ft_pa(stack_a, stack_b, 1);
-	if (ft_lstmin(stack_a) != (*stack_a)->content)
-		ft_nb_to_top(stack_a, ft_lstmin(stack_a));
-	return ;
-}
+// 	min_set = ft_lstmin(stack_a);
+// 	len = ft_lstsize(*stack_a);
+// 	chunk = len / 6;
+// 	i = 0;
+// 	while (*stack_a && i <= chunk)
+// 	{
+// 		// printf("\nmin_set = %d\n", min_set);
+// 		// printf("\nI = %d\n", i);
+// 		// printf("chunk = %d\n", chunk);
+// 		if (i == chunk) 
+// 		{
+// 			if (ft_lstmin(stack_b) < ft_lstmin(stack_a))
+// 			{
+// 				min_set = ft_lstmax(stack_b);
+// 				// printf("\nmin_set = %d\n", min_set);
+// 				ft_nb_to_top(stack_b, ft_lstmax(stack_b));
+// 				ft_nb_to_top(stack_a, ft_lstmin(stack_a));
+// 				while (*stack_b)
+// 				{
+// 					ft_pa(stack_a, stack_b, 1);
+// 					i = 0;
+// 				}
+// 				ft_wich_min_set(stack_a, min_set);   // TRIES TO send correct min to top of A
+// 				// print_list(*stack_a, *stack_b);
+// 			}
+// 			if (ft_lstmin(stack_a) < ft_lstmax(stack_b))
+// 			{
+// 				if (ft_lstmin(stack_a) == min_set)
+// 					ft_nb_to_top(stack_a, ft_lstmin(stack_a));  
+// 				else
+// 					ft_wich_min_set(stack_a, min_set);
+// 				// printf("\nHERE\n");
+// 				// print_list(*stack_a, *stack_b);
+// 				i = 0;
+// 			}
+// 			// print_list(*stack_a, *stack_b);
+// 			// chunk += len / 6;
+// 		}
+// 		else
+// 		{
+// 			ft_wich_min_set(stack_a, min_set);   //TRIES TO send correct min to top of A
+// 			// printf("\n BUG?\n");
+// 		}
+// 		if (*stack_b)
+// 		{
+// 			if ((*stack_a)->content < ft_lstmin(stack_b))
+// 				ft_nb_to_top(stack_b, ft_lstmax(stack_b));   //ensure the max is on top before pushing
+// 			if ((*stack_a)->content > ft_lstmin(stack_b) && (*stack_a)->content < ft_lstmax(stack_b))
+// 				ft_nb_to_top_b(stack_b, ft_lesser_than(*stack_b, (*stack_a)->content));   //if between min & max, find right one
+// 			if ((*stack_a)->content > ft_lstmax(stack_b))
+// 				ft_nb_to_top(stack_b, ft_lstmax(stack_b));
+// 		}
+// 		if (!(*stack_b) && !ft_output_check(stack_a, 0))
+// 			return ;
+// 		ft_pb(stack_a, stack_b, 1);
+// 		i++;
+// 		// print_list(*stack_a, *stack_b);
+// 	}
+// 	while (*stack_b)
+// 		ft_pa(stack_a, stack_b, 1);
+// 	if (ft_lstmin(stack_a) != (*stack_a)->content)
+// 		ft_nb_to_top(stack_a, ft_lstmin(stack_a));
+// 	return ;
+// }
 
 // min_set = ft_lstmin(stack_a);
 // if (i == chunk && ft_lstmax(stack_b) < ft_lstmin(stack_a))
